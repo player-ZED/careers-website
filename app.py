8a788cc8fa6254f047cb1031862a16708de27976
Hassan bin Aman <140601000+player-ZED@users.noreply.github.com>
2024-04-18 06:58:51 +0000

    test

--- app.py
+++ app.py
@@ -3,40 +3,42 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Islamabad',
-    'salary': 'Rs. 1,50,000'
-  },
-  {
-    'id': 2,
-    'title': 'Software Eng',
-    'location': 'Lahore',
-    'salary': 'Rs. 50,000'
-  },
-  {
-    'id': 3,
-    'title': 'Security Analyst',
-    'location': 'Karachi',
-    'salary': 'Rs. 1,00,000'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Developer',
-    'location': 'faisalabad',
-    'salary': 'Rs. 90,000'
-  },
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Islamabad',
+        'salary': 'Rs. 1,50,000'
+    },
+    {
+        'id': 2,
+        'title': 'Software Eng',
+        'location': 'Lahore',
+        'salary': 'Rs. 50,000'
+    },
+    {
+        'id': 3,
+        'title': 'Security Analyst',
+        'location': 'Karachi',
+        'salary': 'Rs. 1,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'location': 'faisalabad',
+        'salary': 'Rs. 90,000'
+    },
 ]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',
-                          jobs=JOBS)
+  return render_template('home.html', jobs=JOBS)
+
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
